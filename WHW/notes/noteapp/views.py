0fac82a99c58e4aa2d1c67d9e86aee8940bc11ac
Andrii <123271442+DarthRomanov@users.noreply.github.com>
2023-08-02 20:42:59 +0300

    Add files via upload

---
+++ WHW/notes/noteapp/views.py
@@ -0,0 +1,50 @@
+from django.shortcuts import render
+from .models import Tag
+from .models import Note
+
+# Create your views here.
+def main(request):
+    return render(request, 'noteapp/index.html')
+
+def tag(request):
+    if request.method == 'POST':
+        form = TagForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect(to='noteapp:main')
+        else:
+            return render(request, 'noteapp/tag.html', {'form': form})
+
+    return render(request, 'noteapp/tag.html', {'form': TagForm()})
+from django.urls import path
+from . import views
+
+app_name = 'noteapp'
+
+urlpatterns = [
+    path('', views.main, name='main'),
+    path('tag/', views.tag, name='tag'),
+]
+
+def note(request):
+    tags = Tag.objects.all()
+
+    if request.method == 'POST':
+        form = NoteForm(request.POST)
+        if form.is_valid():
+            new_note = form.save()
+
+            choice_tags = Tag.objects.filter(name__in=request.POST.getlist('tags'))
+            for tag in choice_tags.iterator():
+                new_note.tags.add(tag)
+
+            return redirect(to='noteapp:main')
+        else:
+            return render(request, 'noteapp/note.html', {"tags": tags, 'form': form})
+
+    return render(request, 'noteapp/note.html', {"tags": tags, 'form': NoteForm()})
+...
+
+def detail(request, note_id):
+    note = get_object_or_404(Note, pk=note_id)
+    return render(request, 'noteapp/detail.html', {"note": note})
